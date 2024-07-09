4e6195ac6bf864c63abcee4aff8dcb8fa031d291
Binaryy <ibolarinwa606@gmail.com>
2024-07-09 21:27:01 +0100

    quick fix

--- def_chat2.py
+++ def_chat2.py
@@ -136,7 +136,7 @@ def main():
                 results_1.append({"Text": text, "URL": url})
             processed_results_1 = process_results(results_1, keyword=search_text)
             docs = [x["metadata"]['text'] for x in query_result['matches']]
-            rerank_docs = co.rerank(query=search_intent, documents=docs, top_n=10, model="rerank-english-v2.0")
+            rerank_docs = co.rerank(query=search_text, documents=docs, top_n=10, model="rerank-english-v2.0")
             docs_reranked = [query_result['matches'][result.index] for result in rerank_docs.results]
             results = []
             displayed_urls = set()
